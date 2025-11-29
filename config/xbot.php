c1b872de50e42d24e3c775742a0171308dcb6760
al3x5dev <al3x5.dev@gmail.com>
2025-11-28 23:10:59 -0500

    Configuration file update

--- config/xbot.php
+++ config/xbot.php
@@ -3,9 +3,9 @@
 use Al3x5\LaravelPsr16Cache;
 
 return [
-    'token' => env('BOT_TOKEN'),
-    'admins' => [],
+    'token' => env('BOT_TOKEN', ''),
+    'admins' => env('ADMIN_TELEGRAM_ID', []),
     'cache' => new LaravelPsr16Cache,
-    'debug' => true,
+    'debug' => env('APP_DEBUG', true),
     'abs_path' => base_path(),
 ];
