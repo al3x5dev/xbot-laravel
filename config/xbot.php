1ba0d20b0d4f9dd83e9d36042a003dcd8c536506
al3x5dev <al3x5.dev@gmail.com>
2025-11-28 23:41:53 -0500

    Fix bug

--- config/xbot.php
+++ config/xbot.php
@@ -4,7 +4,7 @@ use Al3x5\LaravelPsr16Cache;
 
 return [
     'token' => env('BOT_TOKEN', ''),
-    'admins' => env('ADMIN_TELEGRAM_ID', []),
+    'admins' => explode(',', env('ADMIN_TELEGRAM_ID', '')),
     'cache' => new LaravelPsr16Cache,
     'debug' => env('APP_DEBUG', true),
     'abs_path' => base_path(),
